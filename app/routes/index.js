604b440d0cacb419c545887933e81e35abc7ded1
Tommy Kwan <tkwan196@gmail.com>
2014-12-17 20:31:56 +0000

    Removed commented out code

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,19 +9,4 @@ fs.readdirSync(__dirname).filter(function (file){
   router.use(routes.mountPath, routes.routes);
 });
 
-/*require('require-all')({
-  dirname: __dirname,
-
-})
-
-var landlordRoutes = require("./landlords");
-var propertyRoutes = require("./properties");
-var authenticateRoutes = require("./authenticate");
-var contractRoutes = require("./contract");
-
-router.use("/landlord", landlordRoutes);
-router.use("/property", propertyRoutes);
-router.use("/authenticate", authenticateRoutes);
-router.use("/contract", contractRoutes);*/
-
 module.exports = router;
