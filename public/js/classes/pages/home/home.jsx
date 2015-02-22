fc13b5c8d45eecee13d304f5f3022d684bb9aa1a
Tommy Kwan <tkwan196@gmail.com>
2015-02-22 17:16:35 +0000

    Added missing file

---
+++ public/js/classes/pages/home/home.jsx
@@ -0,0 +1,14 @@
+var React = require("react");
+var PageHeading = require("../../components/page-heading.jsx");
+
+module.exports = React.createClass({
+
+  render: function() {
+    return (
+        <div>
+          <PageHeading title="Dashboard" />
+        </div>
+    );
+  }
+
+});
